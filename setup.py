fca2bb33388601fa0ad251dca3c5a60969e41d65
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2020-03-23 18:53:49 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='elsys',
+      version='1.0.0',
+      description='My elsys package',
+      url='https://github.com/zlatililova/Python-9th-grade',
+      author='Z Lilova',
+      author_email='zlati.lilova@gmail.com',
+      license='MIT',
+      packages=['GGGG'],
+      zip_safe=False)
